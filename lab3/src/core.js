8a7a2799fefc997c845fb07137d83fa66623427a
ApelsinDev <nik_zhur_serg@mail.ru>
2024-11-15 21:51:03 +0400

    реализовал все функции из задания

--- lab3/src/core.js
+++ lab3/src/core.js
@@ -2,42 +2,67 @@
  * Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
  * @param {*} n
  */
-function isInteger(n) {}
+function isInteger(n) {
+    return (n | 0) === n;
+}
 
 /**
  * Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
  */
-function even() {}
+function even() {
+    return Array.from({length: 10}, (_, i) => (i + 1) * 2);
+}
 
 /**
  * Напишите функцию, считающую сумму чисел до заданного используя цикл
  * @param {*} n
  */
-function sumTo(n) {}
+function sumTo(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
 
 /**
  * Напишите функцию, считающую сумму чисел до заданного используя рекурсию
  * @param {*} n
  */
-function recSumTo(n) {}
+function recSumTo(n) {
+    if (n === 0) return 0;
+    return n + recSumTo(n - 1);
+}
 
 /**
  * Напишите функцию, считающую факториал заданного числа
  * @param {*} n
  */
-function factorial(n) {}
+function factorial(n) {
+    if (n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+}
 
 /**
  * Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
  * @param {*} n
  */
-function isBinary(n) {}
+function isBinary(n) {
+    return n > 0 && (n & (n - 1)) === 0;
+}
 
 /**
  * Напишите функцию, которая находит N-е число Фибоначчи
  * @param {*} n
  */
-function fibonacci(n) {}
+function fibonacci(n) {
+    if (n <= 1) return n;
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        [a, b] = [b, a + b];
+    }
+    return b;
+}
 
 /** Напишите функцию, которая принимает начальное значение и функцию операции
  * и возвращает функцию - выполняющую эту операцию.
@@ -50,7 +75,14 @@ function fibonacci(n) {}
  * console.log(sumFn(5)) - 15
  * console.log(sumFn(3)) - 18
  */
-function getOperationFn(initialValue, operatorFn) {}
+function getOperationFn(initialValue, operatorFn) {
+    let storedValue = initialValue;
+    return function (newValue) {
+        if (!operatorFn) return storedValue;
+        storedValue = operatorFn(storedValue, newValue);
+        return storedValue;
+    };
+}
 
 /**
  * Напишите функцию создания генератора арифметической последовательности.
@@ -68,14 +100,18 @@ function getOperationFn(initialValue, operatorFn) {}
  * console.log(generator()); // 7
  * console.log(generator()); // 9
  */
-function sequence(start, step) {}
+function sequence(start = 0, step = 1) {
+    let current = start - step;
+    return function () {
+        return (current += step);
+    };
+}
 
 /**
  * Напишите функцию deepEqual, которая принимает два значения
  * и возвращает true только в том случае, если они имеют одинаковое значение
  * или являются объектами с одинаковыми свойствами,
  * значения которых также равны при сравнении с рекурсивным вызовом deepEqual.
- * Учитывать специфичные объекты(такие как Date, RegExp и т.п.) не обязательно
  *
  * @param {object} firstObject - первый объект
  * @param {object} secondObject - второй объект
@@ -84,7 +120,35 @@ function sequence(start, step) {}
  * deepEqual({arr: [22, 33], text: 'text'}, {arr: [22, 33], text: 'text'}) // true
  * deepEqual({arr: [22, 33], text: 'text'}, {arr: [22, 3], text: 'text2'}) // false
  */
-function deepEqual(firstObject, secondObject) {}
+function deepEqual(firstObject, secondObject) {
+    // Используем Object.is для проверки на строгое равенство значений
+    // Это корректно обрабатывает случаи NaN === NaN и различие -0 и 0
+    if (Object.is(firstObject, secondObject)) return true;
+
+    // Если хотя бы одно из значений не объект (или равно null), возвращаем false
+    // Пример: числа, строки, булевы значения, null
+    if (
+        typeof firstObject !== 'object' ||
+        typeof secondObject !== 'object' ||
+        firstObject === null ||
+        secondObject === null
+    ) {
+        return false;
+    }
+
+    // Получаем массивы ключей для обоих объектов
+    const keysA = Object.keys(firstObject);
+    const keysB = Object.keys(secondObject);
+
+    // Если количество ключей разное, объекты не равны
+    if (keysA.length !== keysB.length) return false;
+
+    // Проверяем, что каждый ключ из первого объекта есть во втором объекте,
+    // и значения этих ключей равны при рекурсивной проверке deepEqual
+    return keysA.every(
+        key => keysB.includes(key) && deepEqual(firstObject[key], secondObject[key])
+    );
+}
 
 module.exports = {
     isInteger,
